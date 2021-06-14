63bbaba492acef0116d47169e05dbf2403890045
Anawat Jarusiripot <anawat@teohong.com>
2021-06-14 17:42:57 +0700

    list view

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,45 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+
 import '../models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
+
   TransactionList(this.transactions);
+
   @override
   Widget build(BuildContext context) {
     return Container(
-        width: 300,
-        child: SingleChildScrollView(
-            child: Column(
-          children: transactions.map((tx) {
-            return Row(
+      height: 300,
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Card(
+            child: Row(
               children: <Widget>[
                 Container(
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
+                  ),
                   decoration: BoxDecoration(
-                      border: Border.all(color: Colors.purple, width: 2)),
+                    border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                    ),
+                  ),
                   padding: EdgeInsets.all(10),
                   child: Text(
-                    '\$${tx.amount}',
+                    '\$${transactions[index].amount}',
                     style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
+                    ),
                   ),
                 ),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text(tx.title.toString(),
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold)),
-                    Text(DateFormat("dd/MM/yyyy").format(tx.date!),
-                        style: TextStyle(color: Colors.grey)),
+                    Text(
+                      transactions[index].title!,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      DateFormat.yMMMd().format(transactions[index].date!),
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
                   ],
-                )
+                ),
               ],
-            );
-          }).toList(),
-        )));
+            ),
+          );
+        },
+        itemCount: transactions.length,
+      ),
+    );
   }
 }
