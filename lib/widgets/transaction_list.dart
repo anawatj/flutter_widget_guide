84eb7a2f86fac501f42350dead905d4c50296829
Anawat Jarusiripot <anawat@teohong.com>
2021-06-14 17:15:37 +0700

    make list scroller

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -7,36 +7,39 @@ class TransactionList extends StatelessWidget {
   TransactionList(this.transactions);
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((tx) {
-        return Row(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.purple, width: 2)),
-              padding: EdgeInsets.all(10),
-              child: Text(
-                '\$${tx.amount}',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.purple),
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+        width: 300,
+        child: SingleChildScrollView(
+            child: Column(
+          children: transactions.map((tx) {
+            return Row(
               children: <Widget>[
-                Text(tx.title.toString(),
-                    style:
-                        TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-                Text(DateFormat("dd/MM/yyyy").format(tx.date!),
-                    style: TextStyle(color: Colors.grey)),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.purple, width: 2)),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    '\$${tx.amount}',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(tx.title.toString(),
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold)),
+                    Text(DateFormat("dd/MM/yyyy").format(tx.date!),
+                        style: TextStyle(color: Colors.grey)),
+                  ],
+                )
               ],
-            )
-          ],
-        );
-      }).toList(),
-    );
+            );
+          }).toList(),
+        )));
   }
 }
